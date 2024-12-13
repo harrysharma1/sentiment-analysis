dd1955a37b1f63d9db4c64e7090997b0eab72651
Harry Sharma <harrysharma1066@gmail.com>
2024-12-13 19:39:56 +0000

    feat: add get_datasets functionality

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 import os
+import requests
+
 
 class Preprocessing():
-    def __init__(self, url):
-        self.url = url
+    def __init__(self):
+        self.url = {
+            "hate-speech.csv":"https://raw.githubusercontent.com/t-davidson/hate-speech-and-offensive-language/refs/heads/master/data/labeled_data.csv",
+        }
+        
         
         if not os.path.exists(f"{os.getcwd()}/datasets"):
             os.mkdir(f"{os.getcwd()}/datasets")
+    
+    def get_datasets(self):
+        for filename, url in self.url.items():
+            try:
+                response = requests.get(url)
+                path = os.path.join(os.getcwd(), 'datasets', filename) 
+                with open(path, 'wb') as file:
+                    file.write(response.content)
+                print(f"Data saved to: {path}")
+            except requests.HTTPError as e:
+                match e.response.status_code:
+                    case 404:
+                        print(f"Error:\nDataset not found.\nStatus code: {e.response.status_code}.")
+                    case _ :
+                        print(f"Error:\nUnkown.\nStatus code: {e.response.status_code}")
+            except requests.ConnectionError as e:
+                print(f"Error:\nHostname was not resolvable.\nFull error: {e}")
+
+                        
+                        
+a = Preprocessing()
+a.get_datasets()
+
+            
