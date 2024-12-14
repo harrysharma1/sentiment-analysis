c63c3305d4f519b2da2f4c458110ce254ad314e8
Harry Sharma <harrysharma1066@gmail.com>
2024-12-14 22:04:26 +0000

    feat: update url for twitter to include https

--- src/hate_speech_preprocessing.py
+++ src/hate_speech_preprocessing.py
@@ -9,7 +9,7 @@ class HateSpeechUtilities(Utilities):
         tweet = str(tweet)
 
         tweet = re.sub(r'&#8221;', '', tweet)
-        tweet = re.sub(r'http:\/\/t\.co\/[a-zA-Z0-9]+','',tweet)
+        tweet = re.sub(r'https?:\/\/t\.co\/[a-zA-Z0-9]+','',tweet)
         tweet = re.sub(r'^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$', '', tweet) 
         tweet = re.sub(r'@[\w]+', '', tweet) 
         tweet = re.sub(r'RT', '', tweet)
