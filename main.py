1fcdaee04a8643872dd8e4ccf9fdee7d8ce93365
Harry Sharma <harrysharma1066@gmail.com>
2024-12-21 18:38:58 +0000

    upate: remove commented code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from src.train import Preprocessing
 
 a = Preprocessing()
-# a.get_dataset(url="https://raw.githubusercontent.com/BobAdamsEE/SouthParkData/refs/heads/master/by-season/Season-1.csv", filename="skibidi_toilet")
-# a.clean_dataset("skibidi_toilet")
+
 
 a.get_kaggle_dataset("kaggle/hillary-clinton-emails")
